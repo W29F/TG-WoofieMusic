1a0f5454ad77df4a9fbfd16b74933a322e985f4f
W29F <81409844+W29F@users.noreply.github.com>
2021-04-28 15:12:40 +0700

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,13 +9,7 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_text(
         f"""I am **{bn}** !!
-I let you play music in your group's voice chat 😉
-The commands I currently support are:
-⚜️ /play - __Plays the replied audio file or YouTube video through link.__
-⚜️ /pause - __Pause Voice Chat Music.__
-⚜️ /resume - __Resume Voice Chat Music.__
-⚜️ /skip - __Skips the current Music Playing In Voice Chat.__
-⚜️ /stop - __Clears The Queue as well as ends Voice Chat Music.__
+PRIVATE BOT !!
         """,
         reply_markup=InlineKeyboardMarkup(
             [
